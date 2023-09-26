32e5f3f03c6216be55d2bcaf342db96af5fe7e4d
zx20student043 <145435374+zx20student043@users.noreply.github.com>
2023-09-26 13:42:15 +0300

    tercera version(multiplicacion)

--- calculadora.java
+++ calculadora.java
@@ -26,6 +26,16 @@ public class CalculadoraBasica {
         System.out.println("El resultado de la resta es: " + resultado);
 
 
+        System.out.println("Calculadora Básica - Mulriplicacion");
+        System.out.print("Ingrese el primer número: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.print("Ingrese el segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        double resultado = numero1 * numero2;
+        System.out.println("El resultado de la multiplicacion es: " + resultado);
+
         scanner.close();
     }
 }
