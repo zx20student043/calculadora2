5b949365eb83f1f5f820fb5e4500d1089e4c0cf3
zx20student043 <145435374+zx20student043@users.noreply.github.com>
2023-09-26 13:40:20 +0300

    segunda version(resta)

--- calculadora.java
+++ calculadora.java
@@ -15,6 +15,17 @@ public class CalculadoraBasica {
         double resultado = numero1 + numero2;
         System.out.println("El resultado de la suma es: " + resultado);
 
+        System.out.println("Calculadora Básica - Resta");
+        System.out.print("Ingrese el primer número: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.print("Ingrese el segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        double resultado = numero1 - numero2;
+        System.out.println("El resultado de la resta es: " + resultado);
+
+
         scanner.close();
     }
 }
