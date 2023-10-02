efb2b6f9dbfbfb871f58f3e0f56afceb8303f57d
zx20student043 <145435374+zx20student043@users.noreply.github.com>
2023-10-02 11:45:05 +0300

    division

--- calculadora.java
+++ calculadora.java
@@ -26,6 +26,27 @@ public class CalculadoraBasica {
         System.out.println("El resultado de la resta es: " + resultado);
 
 
+        System.out.println("Calculadora Básica - Multiplicacion");
+        System.out.print("Ingrese el primer número: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.print("Ingrese el segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        double resultado = numero1 * numero2;
+        System.out.println("El resultado de la multiplicacion es: " + resultado);
+
+ System.out.println("Calculadora Básica - Division");
+        System.out.print("Ingrese el primer número: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.print("Ingrese el segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        double resultado = numero1 / numero2;
+        System.out.println("El resultado de la division es: " + resultado);
+
+
         scanner.close();
     }
 }
