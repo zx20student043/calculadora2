b4e16e4a35d23a67a68f0a3d626d26b58159e1c4
zx20student043 <145435374+zx20student043@users.noreply.github.com>
2023-10-03 08:29:53 +0300

    cambioSuma

--- calculadora.java
+++ calculadora.java
@@ -5,7 +5,7 @@ public class CalculadoraBasica {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Calculadora Básica - Suma");
+        System.out.println("Calculadora Básica - SUMA");
         System.out.print("Ingrese el primer número:  ");
         double numero1 = scanner.nextDouble();
 
@@ -13,7 +13,7 @@ public class CalculadoraBasica {
         double numero2 = scanner.nextDouble();
 
         double resultado = numero1 + numero2;
-        System.out.println("El resultado de la suma es: " + resultado);
+        System.out.println("La solución de la suma es: " + resultado);
 
         System.out.println("Calculadora Básica - Resta");
         System.out.print("Ingrese el primer número: ");
